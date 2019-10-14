37bca52f33ee9800f1414a9822ac536e5f3703ae
glpla <1942194789@qq.com>
2019-10-14 11:18:19 +0800

    dairy

--- miniprogram/pages/agenda/agenda.js
+++ miniprogram/pages/agenda/agenda.js
@@ -1,66 +1,28 @@
-// pages/agenda/agenda.js
+const db = wx.cloud.database();
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    records: []
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  previewImg(e) {
+    let src = e.currentTarget.dataset.src;
+    let imgs = e.currentTarget.dataset.imgs;
+    console.log(src)
+    wx.previewImage({
+      current: src,
+      urls: imgs
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  onLoad: function(options) {
+    db.collection('daily').orderBy('createTime', 'desc').get()
+      .then(res => {
+        console.log(res)
+        this.setData({
+          records: res.data
+        })
+      })
+      .catch(err => {
+        console.log(err)
+      })
   }
 })
---
+++ miniprogram/pages/dairy/dairy.js
@@ -0,0 +1,99 @@
+import {
+  formatTime
+} from '../../utils/util.js'
+const app = getApp();
+const db = wx.cloud.database();
+Page({
+  data: {
+    msg: '',
+    imgs: [],
+    fileID: []
+  },
+  onChange(e) {
+    console.log(e.detail.value);
+    this.setData({
+      msg: e.detail.value
+    })
+  },
+  uploadImg() {
+    wx.chooseImage({
+      count: 9,
+      sizeType: ['original', 'compressed'],
+      sourceType: ['album', 'camera'],
+      success: res => {
+        // tempFilePath可以作为img标签的src属性显示图片
+        const tempFilePaths = res.tempFilePaths
+        this.setData({
+          imgs: this.data.imgs.concat(tempFilePaths)
+        })
+        // console.log(this.data.imgs)
+      }
+    })
+  },
+  onSubmit() {
+    if (!this.data.msg) {
+      wx.showToast({
+        title: '内容为空',
+      })
+      return;
+    }
+    wx.showToast({
+      title: '提交中',
+    })
+    let promiseAll = [];
+    for (let i = 0; i < this.data.imgs.length; i++) {
+      // console.log(index)
+      promiseAll.push(new Promise((resolve, reject) => {
+        let item = this.data.imgs[i];
+        let ext = /\.\w+$/.exec(item)[0];
+        wx.cloud.uploadFile({
+          cloudPath: 'pla54414-dairy/' + new Date().getTime() + ext,
+          filePath: item,
+          success: res => {
+            // console.log(res.fileID)
+            this.setData({
+              //数组追加哦,否则后面会覆盖前面
+              fileID: this.data.fileID.concat(res.fileID)
+            })
+            //返回处理结果
+            resolve()
+          },
+          fail: console.error
+        })
+      }))
+    }
+    Promise.all(promiseAll).then(res => {
+      //所有的上传都结束了才会执行入库操作
+      // console.log('upload done')
+      db.collection('daily').add({
+          data: {
+            time: formatTime(new Date()),
+            msg: this.data.msg,
+            fileID: this.data.fileID,
+            createTime: db.serverDate()
+          }
+        })
+        .then(res => {
+          wx.hideToast()
+          wx.showToast({
+            title: '提交成功',
+          })
+        })
+        .catch(err => {
+          console.error(err)
+        })
+    }).catch()
+  },
+  onHistory() {
+    wx.navigateTo({
+      url: '../agenda/agenda',
+    })
+  },
+  previewImg(e) {
+    let src = e.currentTarget.dataset.src;
+    wx.previewImage({
+      current: src,
+      urls: this.data.imgs
+    })
+  }
+})
