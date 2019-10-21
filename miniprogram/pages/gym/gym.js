4b1c24e4b0979c686c4165550bf21dc502a78f73
glpla <1942194789@qq.com>
2019-10-22 00:11:21 +0800

    gym quiz

--- miniprogram/pages/gym/gym.js
+++ miniprogram/pages/gym/gym.js
@@ -3,43 +3,51 @@ const db = wx.cloud.database();
 Page({
   data: {
     imgs: [],
-    cIndex: 0,
-    radio0: 0,
-    radio1: 0,
-    radio2: 0,
-    radio3: 0,
-    radio4: 0,
-    score: 0
+    cIndex: -1,
+    radio: [],
+    score: 0,
+    quiz: [],
+    isStart: false
 
   },
   onClose() {
     this.setData({
-      cIndex: 0,
-      score: 0
+      cIndex: -1,
+      radio: [],
+      score: 0,
+      isStart: false
     })
   },
   onStart() {
-    this.setData({
-      cIndex: 300
+    db.collection('pla54414-gym-quiz').get().then(res => {
+      console.log(res.data)
+      this.setData({
+        quiz: res.data,
+        isStart: true,
+        cIndex: 0
+      })
     })
   },
   onChange(e) {
     let ind = e.currentTarget.dataset.ind;
-    let value = e.detail.value
+    let value = e.detail.value;
+    let newValue = 'radio[' + ind + ']';
     this.setData({
-      ['radio' + ind]: value
+      [newValue]: value
     });
   },
-  onNext() {
-    console.log(this.data.radio0)
-    let idx = this.data.cIndex;
-    idx++
+  onIndex(e) {
+    console.log(e.currentTarget.dataset.ind)
     this.setData({
-      cIndex: idx
+      cIndex: e.currentTarget.dataset.ind
     })
   },
   onSubmit() {
-    let sum = Number(this.data.radio0) + Number(this.data.radio1) + Number(this.data.radio2) + Number(this.data.radio3) + Number(this.data.radio4);
+    let sum = 0;
+    for (let i = 0; i < this.data.radio.length; i++) {
+      sum += Number(this.data.radio[i])
+    }
+    console.log(sum)
     this.setData({
       cIndex: 900,
       score: sum
@@ -47,16 +55,22 @@ Page({
   },
   onBack() {
     this.setData({
-      cIndex: 0,
-      score: 0
+      cIndex: -1,
+      score: 0,
+      radio: [],
+      isStart: false
     })
   },
   onLoad: function(options) {
+    wx.showToast({
+      title: '数据加载中',
+    })
     db.collection('pla54414-gym').orderBy('time', 'desc').limit(10).get().then(res => {
-      // console.log(res.data)
+      console.log(res.data)
       this.setData({
         imgs: res.data
       })
+      wx.hideToast();
     })
   }
 })
